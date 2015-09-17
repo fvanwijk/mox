b6a56ffc5da0f5f204c5d6c2bb35ee139d83891a
Frank van Wijk <fvanwijk@xebia.com>
2015-09-17 23:53:35 +0200

    Use the package information from bower in the gruntfile so that the package name is still mox.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('bower.json'),
     paths: {
       src: 'src',
       test: 'test',
