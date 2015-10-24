7fd147ea79d6a3d928e6b1a10337cba331cacdd5
Frank van Wijk <fvanwijk@xebia.com>
2015-10-24 17:03:37 +0200

    Added dependencies and grunt config for creating documentation and view them

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,31 @@ module.exports = function (grunt) {
       dist: 'dist'
     },
     clean: {
-      dist: 'dist',
-      coverage: 'test/coverage'
+      dist: {
+        files: [
+          {
+            dot: true,
+            src: [
+              '<%= paths.dist %>/**/*',
+              '!<%= paths.dist %>/docs'
+            ]
+          }
+        ]
+      },
+      coverage: 'test/coverage',
+      docs: '<%= paths.dist %>/docs/**/*'
+    },
+    connect: {
+      options: {
+        hostname: 'localhost',
+        port: 9001,
+        keepalive: true
+      },
+      docs: {
+        options: {
+          base: '<%= paths.dist %>/docs'
+        }
+      }
     },
     jshint: {
       options: {
@@ -122,11 +145,28 @@ module.exports = function (grunt) {
         commitMessage: 'Bump version to v%VERSION%',
         push: false
       }
+    },
+    ngdocs: {
+      options: {
+        dest: '<%= paths.dist %>/docs',
+        scripts: [
+          'angular.js',
+          'dist/mox.js'
+        ],
+        html5Mode: false,
+        bestMatch: true,
+        title: 'Mox'
+      },
+      all: {
+        title: 'Documentation',
+        src: 'src/**/*.js'
+      }
     }
   });
 
-  grunt.registerTask('build', ['clean', 'test', 'copy', 'concat', 'uglify']);
-  grunt.registerTask('test', ['jscs', 'jshint', 'lintspaces', 'jsonlint', 'karma', 'coverage']);
+  grunt.registerTask('build', ['clean:dist', 'test', 'copy', 'concat', 'uglify']);
+  grunt.registerTask('test', ['jscs', 'jshint', 'lintspaces', 'jsonlint', 'clean:coverage', 'karma', 'coverage']);
+  grunt.registerTask('docs', ['clean:docs', 'ngdocs']);
   grunt.registerTask('default', ['build']);
 
 };
