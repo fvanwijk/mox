99f19aca160e6ab06b8ca6f7e811eb72b423cc71
Frank van Wijk <fvanwijk@xebia.com>
2016-07-14 21:43:05 +0200

    Add npmignore and improve gitignore. Only package contains dist folder.

--- dist/moxConfig.js
+++
@@ -1,8 +0,0 @@
-angular.extend(moxConfig, {
-  // Insert mock factory function, eg.
-  // FooService: mox.createMock('FooService', ['getBars']),
-  // BazService: function ($provide) {
-  //   var mock = jasmine.createSpyObj('BazService', ['getSomething']);
-  //   mox.save($provide, 'BazService', mock);
-  // }
-});
